0d4830d2b979ac35e4743c025aee8c37f50a1013
kotlin <kotlinken@gmail.com>
2022-06-25 16:45:03 +0900

    contains change target

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,18 @@ public class Main {
         List<String> stringList = new ArrayList<>(Arrays.asList("apple", "banana", "corn"));
 
         //soutc -> system.out::println
-        stringList.stream().filter("ba"::contains).forEach(System.out::println);
-        //not working
+        stringList.stream().filter("corn super"::contains).forEach(System.out::println);
 
         stringList.stream().filter(fruit -> fruit.contains("ba")).forEach(System.out::println);
-        //working
 
         stringList.stream().filter("banana"::equals).forEach(System.out::println);
 
+
+        /**
+         * Double colon
+         * can use inside lambda expressions
+         * [instance]::Method or [instance]::new
+         * examples, System.out::println
+         */
     }
 }
